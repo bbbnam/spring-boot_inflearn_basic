ae3ae0194f90df4e26c24b5680fdf4fb81a8604a
bbbnam <bbbnam@naver.com>
2020-10-30 00:59:03 +0900

    feat : static, template-engine, api

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,37 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello JR!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //http의 헤더부와 바디 부분 중 바디 부분에 데이터를(리턴값) 직접 넣어주겠다 라는 의미
+    public String helloString(@RequestParam("name") String name) {
+        return "hello "+ name; //"hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello { //getter, setter 쓰는게 ->자바빈 표준방식 (다른표현 : 프로퍼티 접근방식)
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
