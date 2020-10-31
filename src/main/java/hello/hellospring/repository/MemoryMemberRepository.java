9cca73f87f7a292643c3b38e4eba70fea490c708
bbbnam <bbbnam@naver.com>
2020-11-01 01:49:40 +0900

    test : memberRepository + test code

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -6,8 +6,8 @@ import java.util.*;
 
 public class MemoryMemberRepository implements MemberRepository{
 
-    private static Map<Long, Member> store = new HashMap<>();
-    private static long sequence = 0L;
+    private static Map<Long, Member> store = new HashMap<>(); //실무에서는 동시성 문제로 인해 concurrenthashmap 써야함
+    private static long sequence = 0L; // 이것도 동시성 문제로 AtomicLong 써야함
 
     @Override
     public Member save(Member member) {
@@ -31,5 +31,9 @@ public class MemoryMemberRepository implements MemberRepository{
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
     }
 }
